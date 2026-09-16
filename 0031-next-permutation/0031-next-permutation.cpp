45fa2426ae8632795880b8c095c390461b8d224f
Ramandeep Singh <150070612+Raman8093@users.noreply.github.com>
2026-09-17 02:58:13 +0530

    Time: 0 ms (100%), Space: 15.8 MB (15.1%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int pivot=-1;
-        int pindex=0;
-        int temp=0;
-        int tindex=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]<nums[i+1]){
-                pivot=nums[i];
-                pindex=i;
-            }
-        }
-        for(int i=nums.size()-1;i>pindex;i--){
-            if(nums[i]>nums[pindex]){
-                temp=nums[i];
-                tindex=i;
-                break;
-            }
-        }
-        if(pivot==-1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        swap(nums[pindex],nums[tindex]);
-        reverse(nums.begin()+pindex+1,nums.end());
+        int l=0,r=nums.size()-1;
         
+        // for(int i=nums.size()-1;i>0;i--){
+        //     if(nums[i-1]<nums[i]){
+        //         l=i-1;
+        //         break;
+        //     }
+        // }
+        // while(l<r){
+        //     if(nums[l]<nums[r]){
+        //         swap(nums[l],nums[r]);
+        //         l++;
+        //     }
+        //     else{
+        //         swap(nums[l],nums[r]);
+        //         return;
+        //     }
+           
+        // }
+      
+        // return;
+        next_permutation(nums.begin(),nums.end());
+
+
     }
 };
