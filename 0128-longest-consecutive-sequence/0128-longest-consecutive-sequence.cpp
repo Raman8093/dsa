4dcf05c936dd418ac048fbff2dfde954140b34ea
Ramandeep Singh <150070612+Raman8093@users.noreply.github.com>
2026-09-18 01:03:32 +0530

    Time: 83 ms (47.31%), Space: 89 MB (49.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        if(nums.size()==0){
-            return 0;
-        }
-        int c=0;
+        // sort(nums.begin(),nums.end());
+        // if(nums.size()==0){
+        //     return 0;
+        // }
+        // int c=0;
+        // int max=0;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         nums.erase(nums.begin()+i+1);
+        //         i--;
+        //     }
+        // }
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]+1==nums[i+1]){
+        //         c++;
+        //         if(max<c){
+        //             max=c;
+        //         }
+        //     }
+        //     else{
+        //         c=0;
+        //     }
+        // }
+        // return max+1;
+        int c=1;
         int max=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                nums.erase(nums.begin()+i+1);
-                i--;
+        unordered_set<int>st(nums.begin(),nums.end());
+        for(int x : st){
+            c=1;
+            if(st.count(x-1)){
+               
             }
-        }
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]+1==nums[i+1]){
-                c++;
+            else{
+                while(st.count(x+1)){
+                    c++;
+                    x++;
+                    
+                }
                 if(max<c){
                     max=c;
                 }
             }
-            else{
-                c=0;
-            }
+
         }
-        return max+1;
+        return max;
         
     }
 };
